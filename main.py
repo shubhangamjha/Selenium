588b44811d1b4d82eb8c4fd1515387561885984f
Shubhangam Jha <105533871+shubhangamjha@users.noreply.github.com>
2024-02-27 16:34:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import pandas
 
 service = Service(executable_path='chromedriver.exe')
 driver = webdriver.Chrome(service=service)
